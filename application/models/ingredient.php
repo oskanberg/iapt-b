a8a68fc3eea60d8ecbfd78327c3d1673f57297a2
Oliver Skanberg-Tippen <ost500@york.ac.uk>
2014-03-08 12:16:16 +0000

    added ingredient model class

---
+++ application/models/ingredient.php
@@ -0,0 +1,36 @@
+#<?php
+
+class Ingredient extends CI_Model {
+
+    private $name;
+    private $description;
+    private $link;
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function get_name() {
+        return $this->name;
+    }
+
+    public function get_description() {
+        return $this->description;
+    }
+
+    public function get_link() {
+        return $this->link;
+    }
+
+    public function set_name($value) {
+        $this->name = $value;
+    }
+
+    public function set_description($value) {
+        $this->description = $value;
+    }
+
+    public function set_link($value) {
+        $this->link = $value;
+    }
+}
