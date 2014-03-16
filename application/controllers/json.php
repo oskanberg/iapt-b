8db2c5560c8b0a39921923c6e81721e4b6adb3e8
Tim J. Munn <tm538@york.ac.uk>
2014-03-16 20:02:33 +0000

    Revert "ignore local ide files"
    This reverts commit cbce812e774919bc70d4f06ae482481c8889675e.

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -7,62 +7,24 @@ class Json extends CI_Controller {
         // return index of recipes?
     }
 
-    // domain.com/json/recipe/<id>
-    public function recipe($id) {
-
-        $rep =  new Recipe();
-        $rep->where('id', $id)->get(); //load the model data
-
-        $rep->step->get(); //get the steps assoicated with the model
-        $steps = array();
-
-        /** iterate over the steps building nested to be converted to JSON */
-        foreach ($rep->step as $step) { //get the ingredietns assoicated with each step.
-
-            $step->ingredient->get(); //get the ingredents for a step
-            $ingredients = array(); //all the ingredintes for a step array
-
-            foreach($step->ingredient as $in) {
-                $ingredient_data = array (
-                    "id" => $in->id,
-                    "name" => $in->name,
-                    "description" => $in->description,
-                    "link" => $in->link
-                );
-
-                array_push($ingredients,$ingredient_data); //push the current ingredient data into the list of ingredinetns.
-            }
-
-            $step_data = array (
-                "id" => $step->id,
-                "operation" => $step->operation,
-                "ingredients" => $ingredients //ingredients array
-            );
-
-            array_push($steps,$step_data);
+    // domain.com/json/recipe/<id>/<'segmented','narrative','step-by-step'>
+    public function recipe($id, $format) {
+        switch ($format) {
+            case 'segmented':
+                //
+                break;
+            case 'narrative':
+                //
+                break;
+            case 'step-by-step':
+                //
+                break;
+            default:
+                // step-by-step?
+                break;
         }
-
-        $recipe = array(
-            "id" => $rep->id,
-            "title" => $rep->title,
-            "type" => $rep->type,
-            "serves" => $rep->serves,
-            "narrative" => $rep->narrative_recipe,
-            "segmented" => $rep->segmented_recipe,
-            "step_by_step" => $rep->step_by_step_recipe,
-            "steps" => $steps
-        );
-
-        $json = json_encode(array(
-            "recipe" => $recipe,
-            "debug" => array(
-                "version" => "0.0.1",
-                "timestamp" => time()
-                )
-            )
-        );
-
-        header('Content-Type: application/json');
-        echo $json;
+        // return array of data to be jsonified
     }
 }
+
+?>
