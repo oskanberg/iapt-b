2d5b5b4c84d0f208768e45c00a92f940ea12a634
Oliver Skanberg-Tippen <ost500@york.ac.uk>
2014-04-26 11:08:09 +0100

    messy add new views

---
+++ application/views/category_script.js
@@ -0,0 +1,109 @@
+
+$.extend({
+    el: function(el, props, innerHTML) {
+        var $el = $(document.createElement(el));
+        $el.attr(props);
+        if (typeof innerHTML !== 'undefined') {
+            console.log(innerHTML);
+            $el.text(innerHTML);
+        }
+        return $el;
+    }
+});
+
+
+function request_and_build() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'example_json_categories.html',
+    })
+    .done(create_carousels)
+    .fail(function(jqXHR, status, error_thrown) {
+        alert(status);
+        alert(error_thrown);
+        // TODO: implement good usability error thing
+    });
+}
+
+
+var RECIPES_PER_SECTION = 4;
+
+function create_carousels(json_object) {
+    var categories = json_object.categories;
+    var $carousels = $('div#carousels');
+    for (var i = 0, category; category = categories[i++];) {
+
+        // add title
+        $carousels.append(
+            $.el('div', {'id':category.id +'-title', 'class':'container cat-title'})
+        );
+        $('div#' + category.id + '-title').append(
+            '<h2>' + category.name + '</h2>'
+        );
+
+        // add one carousel with one active row
+        var rows = 0;
+        var carousel_id = category.id + '-carousel';
+        var row_id = category.id + '-row-' + rows;
+        var inner_id = category.id + '-inner';
+
+        $carousels.append(
+            $.el('div', {'id':carousel_id, 'class':'carousel slide'}).append(
+                $.el('div', {'id': inner_id, 'class':'carousel-inner'}).append(
+                    $.el('div', {'class':'item active'}).append(
+                        $.el('div', {'id':row_id,'class':'row inner-shadow'})
+                    )
+                )
+            )
+        );
+
+        // add recipes to this carousel
+        var section_recipes_remaining = RECIPES_PER_SECTION;
+        var recipes = category.category_recipes;
+        var $row = $('div#' + row_id);
+        for (var j = 0, recipe; recipe = recipes[j++];) {
+            if (section_recipes_remaining == 0) {
+                rows++;
+                row_id = category.id + '-row-' + rows;
+                $('div#' + inner_id).append(
+                    $.el('div', {'class':'item'}).append(
+                        $.el('div', {'id':row_id,'class':'row inner-shadow'})
+                    )
+                );
+                $row = $('div#' + row_id);
+                section_recipes_remaining = RECIPES_PER_SECTION;
+            }
+            $row.append(
+                $.el('div', {'class':'col-xs-3'}).append(
+                    $.el('a', {'href':'#','class':'thumbnail'}).append(
+                        $.el('img', {'src':'http://placehold.it/300x200'})
+                    ),
+                    $.el('div', {'class':'carousel-caption'}).append(
+                        $.el('a',{'href':'#'}, recipe.title)
+                    )
+                )
+            );
+            section_recipes_remaining--;
+        }
+
+        // add controls
+        var $carousel = $('div#' + carousel_id);
+        $carousel.append(
+            $.el('a', {
+                'class':'left carousel-control',
+                'href':'#'+carousel_id,
+                'data-slide':'prev'
+            }, '<')
+        );
+        $carousel.append(
+            $.el('a', {
+                'class':'right carousel-control',
+                'href':'#'+carousel_id,
+                'data-slide':'next'
+            }, '>')
+        );
+    }
+}
+
+request_and_build();
---
+++ application/views/recipe_script.js
@@ -0,0 +1,107 @@
+
+$.extend({
+    el: function(el, props, innerHTML) {
+        var $el = $(document.createElement(el));
+        $el.attr(props);
+        if (typeof innerHTML !== 'undefined') {
+            $el.text(innerHTML);
+        }
+        return $el;
+    }
+});
+
+var json_object;
+var default_view = 'Narrative';
+
+function request_and_build() {
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'example_json_recipe.html',
+    })
+    .done(function (json_obj) {
+        json_object = json_obj
+        populate_recipe_data(default_view);
+    })
+    .fail(function(jqXHR, status, error_thrown) {
+        alert(status);
+        alert(error_thrown);
+        // TODO: implement good usability error thing
+    });
+}
+
+function populate_recipe_data(format) {
+    var recipe = json_object.recipe;
+    // set title
+    $('h1#recipe-name').text(recipe.title);
+    var ordering = recipe.orderings[format];
+    // add ingredients
+    var ingredients = build_ingredients_array(ordering);
+    for (var i = 0, ingredient; ingredient = ingredients[i++];) {
+            $('ul#ingredients-list').append(
+                $.el('li', {'class' : 'list-group-item'}, ingredient.name)
+            );
+    }
+
+    // add steps
+    var steps = ordering.steps;
+    for (var i = 0, step; step = steps[i++];) {
+        add_step(step);
+    }
+
+}
+
+function add_step(step) {
+    var $method_section = $('div#method');
+    // add a divider
+    $method_section.append(
+        $.el('div', {'class':'container'}).append(
+            $.el('hr', {'class':'row featurette-divider'})
+        )
+    );
+
+    // add step
+    $method_section.append(
+        $.el('div',{'class':'container light-bg rounded-corners outer-shadow padded'}).append(
+            $.el('div',{'class':'row featurette', 'id':step.id + '-row'})
+        )
+    );
+
+    // add title
+    var $step_row = $('div#' + step.id + '-row');
+    $step_row.append(
+        $.el('h1',{},'Step ' + step.id)
+    )
+
+    // add step
+    $step_row.append(
+        $.el('p',{},step.operation)
+    )
+}
+
+function build_ingredients_array(ordering) {
+    var ingredients = [];
+    for (var j = 0, step; step = ordering.steps[j++];) {
+        ingredients = ingredients.concat(step.ingredients);
+    }
+    return ingredients;
+}
+
+$(document).ready(function () {
+    $('a.fmt').on('click', function (event) {
+        event.preventDefault();
+        var format = $(this).text();
+        // remove current ingredients
+        $('ul#ingredients-list').empty();
+        // remove current method
+        $('div#method').empty();
+        // populate with the new format
+        populate_recipe_data(format);
+        // update buttons
+        $('ul.nav-pills li').each( function () {
+            $(this).removeClass('active');
+        });
+        $(this).parent().addClass('active');
+    })
+    request_and_build();
+});
