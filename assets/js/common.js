359c07bef0497e57c244f99a291fda9785390c78
Oliver Skanberg-Tippen <ost500@york.ac.uk>
2014-04-30 00:35:29 +0100

    make font size persistent

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,7 +4,7 @@ String.prototype.startsWith = function (str) {
     return this.slice(0, str.length) == str;
 };
 
-// localstorage only allows string storage
+// localStorage only allows string storage
 // makes handling of booleans annoying
 Storage.prototype.invertBoolValue = function (key) {
     if (this.getItem(key) === 'true') {
@@ -25,6 +25,15 @@ $(document).ready(function () {
             localStorage[key] = default_preferences[key];
         }
     }
+
+    // restore some font size preference
+    if (localStorage['font_size_offset'] === undefined) {
+        localStorage['font_size_offset'] = '0';
+    } else {
+        var offset = parseInt(localStorage['font_size_offset']);
+        change_text_size('offset');
+    }
+
     // add link to header
     $('div.jumbotron .container h1').on('click', function() {
         window.location = 'http://www-student.cs.york.ac.uk/assessments/IAPT/002/01/';
@@ -39,11 +48,19 @@ $(document).ready(function () {
     });
     $('#larger-font').on('click', function (event) {
         event.preventDefault();
-        make_text_larger(3);
+        change_text_size(3);
+        // update preference
+        var offset = parseInt(localStorage['font_size_offset']);
+        offset += 3;
+        localStorage['font_size_offset'] = offset;
     });
     $('#smaller-font').on('click', function (event) {
         event.preventDefault();
-        make_text_smaller(3);
+        change_text_size(-3);
+        // update preference
+        var offset = parseInt(localStorage['font_size_offset']);
+        offset -= 3;
+        localStorage['font_size_offset'] = offset;
     });
 });
 
@@ -65,20 +82,11 @@ function load_style_preferences() {
 
 }
 
-function make_text_larger(amount) {
+function change_text_size(amount) {
     $('body, p, a, li').each(function () {
         var size = $(this).css('font-size');
         size = parseInt(size.replace('px', ''));
         size += amount;
-        $(this).css('font-size', size + 'px');
-    });
-}
-
-function make_text_smaller(amount) {
-    $('body, p, a, li').each(function () {
-        var size = $(this).css('font-size');
-        size = parseInt(size.replace('px', ''));
-        size -= amount;
         $(this).css('font-size', size + 'px');
     });
 }
